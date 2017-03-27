c6229b9d2aa61c52841aee217e82d98540144846
Kyle Schumacher <Kyle@kyles-macbook-pro.local>
2017-03-26 21:04:25 -0700

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Kyle Schumacher on 3/26/17.
+//  Copyright © 2017 Kyle Schumacher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
